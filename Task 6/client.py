24f2f5eb8f4930fb3850249976274fb7cd435d11
Vishal <vx-shal@users.noreply.github.com>
2024-02-19 10:53:23 +0530

    Task 6 modified

--- Task 6/client.py
+++ Task 6/client.py
@@ -4,6 +4,6 @@ c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c.connect(("localhost", 56))
 c.sendall("test.bin".encode())
 
-content = c.recv(1024)
+content = c.recv(1024).decode()
 print(content)
 c.close()
