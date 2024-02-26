92d1f10282806d21a7c9b0daafa8d1abfef34ca9
Vishal <112974492+erhszo@users.noreply.github.com>
2024-02-26 11:08:41 +0530

    Update client.py

--- Task 8/client.py
+++ Task 8/client.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-file_name = input("Enter the name of the binarymfile you want to request: ")
+file_name = input("Enter the name of the binary file you want to request: ")
 s.sendto(file_name.encode(), ("localhost", 59))
 
 data, addr = s.recvfrom(1024)
