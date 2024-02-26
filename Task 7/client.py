e66d587f1575a1b61be74aa0c0c8de5c4eabd561
Vishal <vx-shal@users.noreply.github.com>
2024-02-26 10:24:40 +0530

    Task 7 added

---
+++ Task 7/client.py
@@ -0,0 +1,12 @@
+# client
+import socket
+
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect(("localhost", 57))
+
+file_name = input("Enter the file name: ")
+c.sendall(file_name.encode())
+content = c.recv(1024).decode()
+print(content)
+
+c.close()
---
+++ Task 7/server.py
@@ -0,0 +1,21 @@
+import socket
+import threading
+
+
+def handle_client(conn, addr):
+    file_name = conn.recv(1024).decode()
+    print(f"Client {addr} requested file: {file_name}")
+    with open(file_name, "rb") as file:
+        content = file.read()
+    conn.sendall(content)
+    conn.close()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("localhost", 57))
+s.listen()
+
+while True:
+    conn, addr = s.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
